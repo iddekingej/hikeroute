22625966254a3215d6ae48f751f6e8b4af362d02
iddekingej@lycos.com <iddekingej@lycos.com>
2017-03-06 21:11:54 +0100

    *Cleanup code, Store route summary info in database

---
+++ database/migrations/2017_03_04_170803_alter_routefiles.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterRoutefiles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('routes', function (Blueprint $p_table) {
+            $p_table->float("minlat")->nullable();
+            $p_table->float("maxlat")->nullable();
+            $p_table->float("minlon")->nullable();
+            $p_table->float("maxlon")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('routes', function (Blueprint $p_table) {
+            $p_table->dropColumn("minlat");
+            $p_table->dropColumn("maxlat");
+            $p_table->dropColumn("minlon");
+            $p_table->dropColumn("maxlon");
+        });
+    }
+}
