f0d10c12f975ef764ddd47e48f11029fc74443d7
iddekingej@lycos.com <iddekingej@lycos.com>
2017-03-29 22:05:43 +0200

    * Must drop contraint when changing column

--- database/migrations/2017_03_29_181131_drop_id_location_nn.php
+++ database/migrations/2017_03_29_181131_drop_id_location_nn.php
@@ -14,7 +14,9 @@ class DropIdLocationNn extends Migration
     public function up()
     {
         Schema::table('routetraces', function (Blueprint $p_table) {
+        	$p_table->dropForeign("routetraces_id_location_foreign");
             $p_table->unsignedInteger("id_location")->nullable(true)->change();
+            $p_table->foreign("id_location")->references("id")->on("locations");
         });
     }
 
@@ -26,7 +28,9 @@ class DropIdLocationNn extends Migration
     public function down()
     {
         Schema::table('routetraces', function (Blueprint $p_table) {
+        	$p_table->dropForeign("routetraces_id_location_foreign");
         	$p_table->unsignedInteger("id_location")->nullable(false)->change();
+        	$p_table->foreign("id_location")->references("id")->on("locations");
         });
     }
 }
