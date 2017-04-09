3a4a77a40e3a899b854ceb2a9ab84d6d3711bfb2
iddekingej@lycos.com <iddekingej@lycos.com>
2017-04-09 16:07:37 +0200

    *Update translation

--- resources/views/traces/list.blade.php
+++ resources/views/traces/list.blade.php
@@ -4,7 +4,7 @@ use App\Vc\RouteTracesVC;
 @extends("layouts.pagemenu",["title"=>__("User route traces")])
 @section("pagebody")
 <?php 
-	RouteTraceVC:listTopMenu();
+	RouteTracesVC::listTopMenu();
 	RouteTracesVC::traceListTable($traces,"traces.show");
 ?>
 @endsection
